4dec35c02d08c8404ea1332f015d27bb55471cbb
Naman Jangir <hello.naman0@gmail.com>
2026-01-09 01:48:47 +0530

    api link updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://zenz-backend.onrender.com/api';
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem('token');
